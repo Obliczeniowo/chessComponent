b780a9b5bd0dbb9e7b395e127701476be33df703
Obliczeniowo <zajaczkowskikrzysztofkz@gmail.com>
2019-11-16 14:15:05 +0100

    Create base components structure

---
+++ src/app/chess/chess/chess-field/chess-field.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-chess-field',
+  templateUrl: './chess-field.component.html',
+  styleUrls: ['./chess-field.component.scss']
+})
+export class ChessFieldComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
