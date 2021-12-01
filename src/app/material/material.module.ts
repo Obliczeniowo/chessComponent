d66445c33d0c466c75248e9a62e2840b8706fa98
Obliczeniowo <zajaczkowskikrzysztofkz@gmail.com>
2021-12-01 23:09:04 +0100

    update angular material

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatFormFieldModule, MatSelectModule } from '@angular/material';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 @NgModule({
